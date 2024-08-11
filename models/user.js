1f25e713bc963235ce1936452eff70c3f6bc2127
ParthaSarathiRautaray1 <parthasarathirautaray176@gmail.com>
2024-08-11 16:17:11 +0530

    env confirm

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
+const mongoURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/testapp1";
 
-mongoose.connect("mongodb://127.0.0.1:27017/testapp1")
-
+mongoose.connect(mongoURI, { useNewUrlParser: true , useUnifiedTopology: true})
 const userSchema = mongoose.Schema({
     image: String,
     email: String,
