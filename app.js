fd823c199afc1ccbce63ea23c625a2842fb04c9e
ParthaSarathiRautaray1 <parthasarathirautaray176@gmail.com>
2024-08-11 15:21:14 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const userModel = require('./models/user')
+
+app.set("view engine", "ejs")
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req,res) => {
+    res.render("index")
+})
+
+app.get('/read',async (req,res) => {
+    let allusers = await userModel.find()
+    res.render("read", {users: allusers})
+})
+
+app.post('/create', async (req,res) => {
+  
+    //creating a dynamic user when a user put information and press submit
+
+    let {name ,email, image} = req.body
+
+    let createdUser = await userModel.create({
+        name : name,
+        email : email,
+        image : image
+    })
+
+    res.redirect("/read")
+
+})
+app.get('/delete/:id',async (req,res) => {
+    let dynamicuser = await userModel.findOneAndDelete({_id: req.params.id})
+    res.redirect("/read",)
+})
+
+
+app.listen(3000)
---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb://127.0.0.1:27017/testapp1")
+
+const userSchema = mongoose.Schema({
+    image: String,
+    email: String,
+    name : String
+})
+
+
+module.exports = mongoose.model('user', userSchema)
